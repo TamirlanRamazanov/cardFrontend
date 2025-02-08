570e5edcfa04ac1d82615892981219b5b652732c
Grandmaster <tamirlan.ramazanov@gmail.com>
2025-02-09 03:05:12 +0500

    creating background and template for future card size

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="container">
+      <div className="rectangle"></div>
+    </div>
+  );
+}
+
+export default App; 
